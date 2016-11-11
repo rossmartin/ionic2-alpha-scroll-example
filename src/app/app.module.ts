ddc6126ff05e6c28703cd38ac2f110402a5e430e
Ross Martin <rmartin311@gmail.com>
2016-11-10 20:37:23 -0600

    Update Ionic to RC2 and use 1.0.5 for component.

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { AlphaListPage } from '../pages/alpha-list/alpha-list';
+import { IonAlphaScrollModule } from 'ionic2-alpha-scroll';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    AlphaListPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+    IonAlphaScrollModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    AlphaListPage
+  ],
+  providers: []
+})
+export class AppModule {}
